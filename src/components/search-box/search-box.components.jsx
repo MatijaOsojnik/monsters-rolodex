e6acbb71be6774ec48bcb60fd7804344eb11486f
Matija Osojnik <matija1.osojnik@gmail.com>
2019-11-15 13:29:37 +0100

    adding files for github pages

---
+++ src/components/search-box/search-box.components.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import '../search-box/search-box.styles.css';
+
+export const SearchBox = ({ placeHolder, changeHandler }) => (
+  <input className="search" type="search" placeholder={placeHolder} onChange={changeHandler} />
+);
