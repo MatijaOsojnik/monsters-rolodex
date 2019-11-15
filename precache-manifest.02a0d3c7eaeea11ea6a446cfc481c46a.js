10d0931a98dd822ce3a16cab4bc10e0e2548d3f3
Matija Osojnik <matija1.osojnik@gmail.com>
2019-11-15 13:28:51 +0100

    Updates

---
+++ precache-manifest.02a0d3c7eaeea11ea6a446cfc481c46a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26b8f7e382b3c1bf523a41296b73c31",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "56d795beb393efa3c906",
+    "url": "/monsters-rolodex/static/css/main.cfbfe2ef.chunk.css"
+  },
+  {
+    "revision": "7529e79d3c7e5260276c",
+    "url": "/monsters-rolodex/static/js/2.24beb491.chunk.js"
+  },
+  {
+    "revision": "56d795beb393efa3c906",
+    "url": "/monsters-rolodex/static/js/main.b77678f4.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
